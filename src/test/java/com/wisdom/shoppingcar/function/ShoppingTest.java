bdd5bc590f7cf195c070c0926c902781d2101c04
whitefree <773625185@qq.com>
2016-05-14 13:34:41 +0700

    add first test case

---
+++ src/test/java/com/wisdom/shoppingcar/function/ShoppingTest.java
@@ -0,0 +1,13 @@
+package com.wisdom.shoppingcar.function;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShoppingTest 
+{
+    @Test
+    public void shouldShoppingSuccessfulyWhenCaseA()
+    {
+    	Assert.assertTrue(true);
+    }
+}
