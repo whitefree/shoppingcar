886652f60a7cfb82757f3adeb19141841e132a36
whitefree <773625185@qq.com>
2016-05-14 11:03:15 +0700

    first commit: add project structure

---
+++ src/main/java/com/wisdom/shoppingcar/App.java
@@ -0,0 +1,13 @@
+package com.wisdom.shoppingcar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
